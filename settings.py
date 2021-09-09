646b92bfa91c6f36fca028f38b7fe00a9945e48a
Assad <assadullahbaig3@gmail.com>
2021-09-09 20:09:37 +0500

    alien invasion practice

--- settings.py
+++ settings.py
@@ -7,10 +7,17 @@ class Settings:
        self.screen_height = 800
        self.bg_color = (230, 230, 230) 
         #ship settings.
-       self.ship_speed = 1.5
+       self.ship_speed = 2.0
        #bullet settings.
-       self.bullet_speed = 1.0
+       self.bullet_speed = 5.0
        self.bullet_width = 3
        self.bullet_height = 15
        self.bullet_color = (60, 60, 60)
-       self.bullets_allowed = 3
+       self.bullets_allowed = 99
+       #alien settings.
+       self.alien_speed = 1.0
+       self.fleet_drop_Speed = 10
+       #fleet_direct of 1 represents right; -1 represents left.
+       self.fleet_direction = 1
+
+
