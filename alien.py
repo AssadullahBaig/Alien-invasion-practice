3f898ea4d80071a596147e5aba95245a9dc3966d
Assad <assadullahbaig3@gmail.com>
2021-10-16 18:54:28 +0500

    Ch 13 complete( adding aliens and shooting them down)

--- alien.py
+++ alien.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
-
-
+ 
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
@@ -11,15 +10,15 @@ class Alien(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-    # Load the alien image and set its rect attribute.
+        # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-    # Start each new alien near the top left of the screen.
+        # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-    # Store thet alien's exact horizontal position/
+        # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
 
     def check_edges(self):
@@ -29,7 +28,7 @@ class Alien(Sprite):
             return True
 
     def update(self):
-        """Move the alien to the right."""
+        """Move the alien right or left."""
         self.x += (self.settings.alien_speed *
-                   self.settings.fleet_direction)
+                        self.settings.fleet_direction)
         self.rect.x = self.x
--- bullet.py
+++ bullet.py
@@ -11,18 +11,20 @@ class Bullet(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
                                 self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
-    # Store the bullet's position as a decimal value.
+        # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
 
     def update(self):
         """Move the bullet up the screen."""
-    # Update the decimal position of the bullet.
+        # Update the decimal position of the bullet.
         self.y -= self.settings.bullet_speed
-    # updatet the rect position.
+        # Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
